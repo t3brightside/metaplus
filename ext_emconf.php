b6f1df28ace7722acd97ab447ff6e5e1b4254537
Tanel Põld <tanel@brightside.ee>
2018-04-12 12:53:41 +0800

    Cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,41 +1,27 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "metaplus".
- *
- * Auto generated | Identifier: 4a25e6bdbd896f3d7a4721a7aea3cc5a
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Meta Plus',
-	'description' => 'Control over page title prefix and suffix, OpenGraph image and Favicon.',
-	'category' => 'fe',
-	'version' => '0.0.5',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearcacheonload' => true,
-	'author' => 'Tanel Põld',
-	'author_email' => 'tanel@brightside.ee',
-	'author_company' => 'Brightside OÜ / t3brightside.com',
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
-			'typo3' => '8.7.0 - 9.9.99',
-		),
-		'conflicts' =>
-		array (
-		),
-		'suggests' =>
-		array (
-		),
-	),
-	'_md5_values_when_last_written' => 'a:39:{s:14:"Configuration/";s:4:"d41d";s:18:"Configuration/TCA/";s:4:"d41d";s:28:"Configuration/TCA/Overrides/";s:4:"d41d";s:44:"Configuration/TCA/Overrides/sys_template.php";s:4:"d447";s:9:"__MACOSX/";s:4:"d41d";s:23:"__MACOSX/Configuration/";s:4:"d41d";s:27:"__MACOSX/Configuration/TCA/";s:4:"d41d";s:37:"__MACOSX/Configuration/TCA/Overrides/";s:4:"d41d";s:55:"__MACOSX/Configuration/TCA/Overrides/._sys_template.php";s:4:"ebb9";s:37:"Configuration/TCA/Overrides/pages.php";s:4:"48bb";s:48:"__MACOSX/Configuration/TCA/Overrides/._pages.php";s:4:"ebb9";s:38:"__MACOSX/Configuration/TCA/._Overrides";s:4:"ebb9";s:28:"__MACOSX/Configuration/._TCA";s:4:"ebb9";s:25:"Configuration/TypoScript/";s:4:"d41d";s:33:"Configuration/TypoScript/setup.ts";s:4:"6459";s:34:"__MACOSX/Configuration/TypoScript/";s:4:"d41d";s:44:"__MACOSX/Configuration/TypoScript/._setup.ts";s:4:"ebb9";s:35:"__MACOSX/Configuration/._TypoScript";s:4:"ebb9";s:24:"__MACOSX/._Configuration";s:4:"ebb9";s:14:"ext_tables.php";s:4:"b6d3";s:25:"__MACOSX/._ext_tables.php";s:4:"ebb9";s:10:"Resources/";s:4:"d41d";s:18:"Resources/Private/";s:4:"d41d";s:27:"Resources/Private/Language/";s:4:"d41d";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"0e19";s:19:"__MACOSX/Resources/";s:4:"d41d";s:27:"__MACOSX/Resources/Private/";s:4:"d41d";s:36:"__MACOSX/Resources/Private/Language/";s:4:"d41d";s:54:"__MACOSX/Resources/Private/Language/._locallang_db.xml";s:4:"ebb9";s:37:"__MACOSX/Resources/Private/._Language";s:4:"ebb9";s:28:"__MACOSX/Resources/._Private";s:4:"ebb9";s:20:"__MACOSX/._Resources";s:4:"ebb9";s:12:"ext_icon.png";s:4:"8dce";s:23:"__MACOSX/._ext_icon.png";s:4:"ebb9";s:25:"__MACOSX/._ext_emconf.php";s:4:"ebb9";s:14:"ext_tables.sql";s:4:"ea42";s:25:"__MACOSX/._ext_tables.sql";s:4:"ebb9";s:17:"ext_localconf.php";s:4:"cfef";s:28:"__MACOSX/._ext_localconf.php";s:4:"ebb9";}',
-);
-
-?>
+	$EM_CONF[$_EXTKEY] = array (
+		'title' => 'Meta Plus',
+		'description' => 'Control over page title prefix and suffix, OpenGraph image and Favicon.',
+		'category' => 'fe',
+		'version' => '0.0.5',
+		'state' => 'stable',
+		'uploadfolder' => false,
+		'createDirs' => '',
+		'clearcacheonload' => true,
+		'author' => 'Tanel Põld',
+		'author_email' => 'tanel@brightside.ee',
+		'author_company' => 'Brightside OÜ / t3brightside.com',
+		'constraints' =>
+			array (
+				'depends' =>
+				array (
+					'typo3' => '8.7.0 - 9.9.99',
+				),
+				'conflicts' =>
+				array (
+				),
+				'suggests' =>
+				array (
+				),
+			),
+		);
